75c11e46fa8264250bed87915bc44d51329028fc
Grayson Pike <graysonpike@Graysons-MBP.attlocal.net>
2015-08-16 15:05:02 -0500

    Added pt 5

---
+++ ch3/5/main.c
@@ -0,0 +1,13 @@
+// Program to print the sums of cols, rows, and diags in a 4x4 number square
+
+// Because the book hasn't covered arrays,
+// I'll return to this exercise when I can slove it properly.
+// I think they want me to create 16 different variables at this point
+// which is really dumb.
+
+#include <stdio.h>
+
+int main(void){
+	// To be continued...
+	return 0;
+}
