7a7ad0b3d0c5f9f1c5883a57296245dc33ff2910
Grayson112233 <graysonpike@gmail.com>
2016-05-12 21:18:07 -0400

    Using pointers to pointers

--- ch17/ordered_linked/main.c
+++ ch17/ordered_linked/main.c
@@ -8,44 +8,44 @@ struct node {
 	struct node *next;
 };
 
-struct node *add_node(int value, struct node *first) {
+void add_node(int value, struct node **first) {
 	// Create new node for insertion
 	struct node *temp = malloc(sizeof(struct node));
 	temp->value = value;
 	temp->next = NULL;
 	// Working pointers
 	struct node *previous = NULL;
-	struct node *current = first;
+	struct node *current = *first;
 	//struct node *next = first->next;
 	// Is there only one node in the list?
 	if(current->next == NULL){
 		if(current->value > value) {
 			temp->next = current;
-			return temp;
+			*first = temp;
 		} else {
 			current->next = temp;
-			return current;
 		}
+	} else {
+		// Otherwise, search the list and insert the node where it belongs
+		while(current->next != NULL && current->value <= value) {
+			previous = current;
+			current = current->next;
+		}
+		// Insert node between previous and current
+		previous->next = temp;
+		temp->next = current;
 	}
-	// Otherwise, insert the new node in the list before
-	while(current->next != NULL && current->value <= value) {
-		previous = current;
-		current = current->next;
-	}
-	previous->next = temp;
-	temp->next = current;
-	return first;
 }
 
 // Remove a node by its value. If multiple nodes of the same value
 // exist, remove all of them.
-struct node *remove_node(int value, struct node *first) {
-	struct node *current = first;
+void remove_node(int value, struct node **first) {
+	struct node *current = *first;
 	struct node *previous = NULL;
 	while(current != NULL) {
 		if(current->value == value && previous == NULL) {
-			first = current->next;
-			current = first;
+			*first = current->next;
+			current = *first;
 		} else {
 			if(current->value == value){
 				previous->next = current->next;
@@ -57,12 +57,13 @@ struct node *remove_node(int value, struct node *first) {
 			}
 		}
 	}
-	return first;
 }
 
 void print_list(struct node *first) {
+	int i = 0;
 	struct node *current = first;
-	while(current != NULL) {
+	while(current != NULL && i < 10) {
+		i += 1;
 		printf("%d\n", current->value);
 		current = current->next;
 	}
@@ -78,21 +79,20 @@ int main() {
 	first->next = NULL;
 	first->value = 1;
 	printf("Starting list: \n");
-	print_list(first);
+	//print_list(first);
 	// Begin adding values out of order
-	first = add_node(3, first);
-	first = add_node(2, first);
+	add_node(3, &first);
+	add_node(2, &first);
 	printf("After adding values:\n");
 	print_list(first);
 	// Remove all nodes with a value of 2
-	remove_node(2, first);
+	remove_node(2, &first);
 	printf("After removing '2':\n");
 	print_list(first);
-	// Add another '1', one of which already exists in the array
-	add_node(1, first);
+	// Add another '1', one of which already exists in the list
+	add_node(1, &first);
 	printf("After adding another '1':\n");
 	print_list(first);
-	
 	return 0;
 	
 }
